0ba4489c36dc5d975feef1d9ea70e957b1d21a3a
paul.rutledge <paul.rutledge@vodori.com>
2020-02-22 12:38:33 -0600

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.rutledgepaulv/topofuncs "0.1.0-SNAPSHOT"
+(defproject org.clojars.rutledgepaulv/topofuncs "0.1.0"
 
   :description
   "Useful function compositions from graphs."
